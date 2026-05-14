41d3aa287b78a7b34da22b822b3a789732b85d8f
vaishnavikatkar4037-design <vaishnavikatkar4037@gmail.com>
2026-05-14 15:23:22 +0530

    Create expt4.2.py

---
+++ expt 4/expt4.2.py
@@ -0,0 +1,6 @@
+# Take input from the user
+string = input("Enter a string: ")
+# Reverse the string using slicing
+reversed_string = string[::-1]
+# Print the result
+print("Reversed string:", reversed_string)
