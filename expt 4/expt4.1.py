48d23aec9e6bbcc59fa5ce8d055be081bc31504b
vaishnavikatkar4037-design <vaishnavikatkar4037@gmail.com>
2026-05-14 15:22:54 +0530

    Create expt4.1.py

---
+++ expt 4/expt4.1.py
@@ -0,0 +1,12 @@
+# Taking list input from the user
+n = int(input("Enter number of elements: "))
+numbers = []
+for i in range(n):
+   num = int(input(f"Enter element {i+1}: "))
+   numbers.append(num)
+# Calculating sum and average
+total = sum(numbers)
+average = total / n if n > 0 else 0
+print("List:", numbers)
+print("Sum of elements:", total)
+print("Average of elements:", average)
